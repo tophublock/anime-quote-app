21a8e7c693ca25d8c6a53b7bf2e094383227a1d6
Aim4 <aimforking@gmail.com>
2020-07-22 22:39:29 -0400

    Add jquery and use test ajax query

---
+++ src/main.js
@@ -0,0 +1,16 @@
+$.ajax({
+    url: 'https://anime-chan.herokuapp.com/api/quotes/random',
+    type: 'GET',
+    dataType: 'json',
+    async: true,
+    success: (data) => {
+        console.log('hello');
+        console.log(data);
+    },
+    error: () => {
+        console.log('fail');
+    },
+    complete: () => {
+        console.log('always');
+    },
+});
