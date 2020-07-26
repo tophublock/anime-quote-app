397a69e3fdec22314d847a32d6f7438e8644eb24
Aim4 <aimforking@gmail.com>
2020-07-26 16:55:33 -0400

    Create quote cards and add dynamically to page

--- src/constants.js
+++ src/constants.js
@@ -12,3 +12,9 @@ export const JSON = 'json';
 export const ANIME_FIELD = 'anime';
 export const CHARACTER_FIELD = 'character';
 export const QUOTE_FIELD = 'quote';
+
+// Classes
+export const AUTHOR_CLASS = 'author';
+export const QUOTE_CARD_CLASS = 'quote-card';
+export const QUOTE_CLASS = 'quote';
+export const SOURCE_CLASS = 'source';
--- src/quote.js
+++ src/quote.js
@@ -1,8 +1,25 @@
+import * as CONST from './constants.js';
+
 export default class Quote {
     constructor(quote, author, source) {
         this._quote = quote;
         this._author = author;
         this._source = source;
+    }
+
+    render() {
+        const $card = $('<div>', { class: CONST.QUOTE_CARD_CLASS });
+
+        // Inner card elements
+        const $quote = $('<div>', { class: CONST.QUOTE_CLASS });
+        $quote.text(this._quote);
+        const $author = $('<div>', { class: CONST.AUTHOR_CLASS });
+        $author.text(this._author);
+        const $source = $('<div>', { class: CONST.SOURCE_CLASS });
+        $source.text(this._source);
+
+        $card.append($quote).append($author).append($source);
+        return $card;
     }
 
     get quote() {
